45ec9c1d4444b83598752215b000bc0ed26f59d6
sunayana <sunayana.sarangi@gmail.com>
2023-12-29 16:19:54 +0100

    using just vercel_url

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import TicketCard from "./(components)/TicketCard";
 const getTickets = async () => {
   try {
     //const res = await fetch("http://localhost:3000/api/Tickets", {
-    const res = await fetch("/api/Tickets", {
+    const res = await fetch(`https://${process.env.VERCEL_URL}/api/Tickets`, {
       cache: "no-store",
     });
     return res.json();
